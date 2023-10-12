c2d1dde88d07ceb085de11456e512449087fdc32
Oliver Skroblin <o.skroblin@shopware.com>
2023-10-12 10:29:11 +0200

    feat: added util classes for mysql to handle transactions and multi insert queries

---
+++ src/Mysql/Util/MultiInsert.php
@@ -0,0 +1,166 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Storage\MySQL\Util;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\ParameterType;
+
+class MultiInsert
+{
+    /**
+     * @var array<string, array{data: array<string, mixed>, columns: list<string>, }>
+     */
+    private array $inserts = [];
+
+    /**
+     * @var int<1, max>
+     */
+    private readonly int $chunkSize;
+
+    public function __construct(
+        private readonly Connection $connection,
+        int $chunkSize = 250,
+        private readonly bool $ignore = false,
+        private readonly bool $replace = false
+    ) {
+        if ($chunkSize < 1) {
+            throw new \InvalidArgumentException(
+                sprintf('Parameter $chunkSize needs to be a positive integer starting with 1, "%d" given', $chunkSize)
+            );
+        }
+        $this->chunkSize = $chunkSize;
+    }
+
+    /**
+     * @param array<string, mixed>      $data
+     * @param array<string, ParameterType::*>|null $types
+     */
+    public function add(string $table, array $data, ?array $types = null): void
+    {
+        $columns = [];
+
+        foreach ($data as $key => &$value) {
+            $columns[] = $key;
+
+            $type = ParameterType::STRING;
+
+            if ($types !== null && isset($types[$key])) {
+                $type = $types[$key];
+            }
+
+            if ($value === null) {
+                $value = 'NULL';
+            } else if (is_array($value)) {
+                $value = $this->connection->quote(json_encode($value, \JSON_UNESCAPED_UNICODE | \JSON_PRESERVE_ZERO_FRACTION | \JSON_THROW_ON_ERROR | \JSON_INVALID_UTF8_IGNORE), $type);
+            } else {
+                $value = $this->connection->quote($value, $type);
+            }
+        }
+
+        $this->inserts[$table][] = [
+            'data' => $data,
+            'columns' => $columns,
+            'types' => $types,
+        ];
+    }
+
+    public function execute(): void
+    {
+        if (empty($this->inserts)) {
+            return;
+        }
+
+        $grouped = $this->prepare();
+        RetryableTransaction::retryable($this->connection, function () use ($grouped): void {
+            foreach ($grouped as $query) {
+                $this->connection->executeStatement($query);
+            }
+        });
+        unset($grouped);
+
+        $this->inserts = [];
+    }
+
+    private function prepare(): array
+    {
+        $queries = [];
+        $template = 'INSERT INTO %s (%s) VALUES %s;';
+
+        if ($this->ignore) {
+            $template = 'INSERT IGNORE INTO %s (%s) VALUES %s;';
+        }
+
+        if ($this->replace) {
+            $template = 'REPLACE INTO %s (%s) VALUES %s;';
+        }
+
+        foreach ($this->inserts as $table => $rows) {
+            $columns = $this->prepareColumns($rows);
+            $data = $this->prepareValues($columns, $rows);
+
+            $columns = array_map(self::escape(...), $columns);
+
+            $chunks = array_chunk($data, $this->chunkSize);
+            foreach ($chunks as $chunk) {
+                $queries[] = sprintf(
+                    $template,
+                    self::escape($table),
+                    implode(', ', $columns),
+                    implode(', ', $chunk)
+                );
+            }
+        }
+
+        return $queries;
+    }
+
+    private function prepareColumns(array $rows): array
+    {
+        $columns = [];
+        foreach ($rows as $row) {
+            foreach ($row['columns'] as $column) {
+                $columns[$column] = 1;
+            }
+        }
+
+        return array_keys($columns);
+    }
+
+    private function prepareValues(array $columns, array $rows): array
+    {
+        $stackedValues = [];
+        /** @var array<string, mixed> $defaults */
+        $defaults = array_combine(
+            $columns,
+            array_fill(0, \count($columns), 'DEFAULT')
+        );
+        foreach ($rows as $row) {
+            $data = $row['data'];
+            $values = $defaults;
+            if (!\is_array($values)) {
+                continue;
+            }
+
+            /**
+             * @var string $key
+             * @var mixed $value
+             */
+            foreach ($data as $key => $value) {
+                $values[$key] = $value;
+            }
+            $stackedValues[] = '(' . implode(',', $values) . ')';
+        }
+
+        return $stackedValues;
+    }
+
+    private static function escape(string $string): string
+    {
+        if (\mb_strpos($string, '`') !== false) {
+            throw new \InvalidArgumentException('Backtick not allowed in identifier');
+        }
+
+        return '`' . $string . '`';
+    }
+
+}
---
+++ src/Mysql/Util/RetryableTransaction.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Shopware\Storage\MySQL\Util;
+
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception\RetryableException;
+
+class RetryableTransaction
+{
+    /**
+     * Executes the given closure inside a DBAL transaction. In case of a deadlock (RetryableException) the transaction
+     * is rolled back and the closure will be retried. Because it may run multiple times the closure should not cause
+     * any side effects outside its own scope.
+     *
+     * @template TReturn of mixed
+     *
+     * @param \Closure(Connection): TReturn $closure
+     *
+     * @return TReturn
+     */
+    public static function retryable(Connection $connection, \Closure $closure)
+    {
+        return self::retry($connection, $closure, 0);
+    }
+
+    /**
+     * @template TReturn of mixed
+     *
+     * @param \Closure(Connection): TReturn $closure The function to execute transactionally.
+     *
+     * @return TReturn
+     */
+    private static function retry(Connection $connection, \Closure $closure, int $counter)
+    {
+        ++$counter;
+
+        try {
+            return $connection->transactional($closure);
+        } catch (RetryableException $retryableException) {
+            if ($connection->getTransactionNestingLevel() > 0) {
+                // If this RetryableTransaction was executed inside another transaction, do not retry this nested
+                // transaction. Remember that the whole (outermost) transaction was already rolled back by the database
+                // when any RetryableException is thrown.
+                // Rethrow the exception here so only the outermost transaction is retried which in turn includes this
+                // nested transaction.
+                throw $retryableException;
+            }
+
+            if ($counter > 10) {
+                throw $retryableException;
+            }
+
+            // Randomize sleep to prevent same execution delay for multiple statements
+            usleep(random_int(10, 20));
+
+            return self::retry($connection, $closure, $counter);
+        }
+    }
+}
