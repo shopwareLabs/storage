c5ed1fd6c4ac7618f7712a4d1dda45cd1cba1e81
oskroblin Skroblin <o.skroblin@shopware.com>
2024-03-04 11:00:07 +0100

    feat: Added storage context as key value storage function parameter

--- src/Common/KeyValue/KeyAware.php
+++ src/Common/KeyValue/KeyAware.php
@@ -4,18 +4,14 @@ namespace Shopware\Storage\Common\KeyValue;
 
 use Shopware\Storage\Common\Document\Document;
 use Shopware\Storage\Common\Document\Documents;
+use Shopware\Storage\Common\StorageContext;
 
 interface KeyAware
 {
     /**
      * @param array<string> $keys
-     * @return Documents
      */
-    public function mget(array $keys): Documents;
+    public function mget(array $keys, StorageContext $context): Documents;
 
-    /**
-     * @param string $key
-     * @return Document|null
-     */
-    public function get(string $key): ?Document;
+    public function get(string $key, StorageContext $context): ?Document;
 }
